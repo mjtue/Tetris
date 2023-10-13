ae6c55493fc8771cce9e8655b34805ec2d214c14
mjtue <147407969+mjtue@users.noreply.github.com>
2023-10-13 14:47:01 +0200

    Create Grid.java
    Class responsible for the blocks falling each second

---
+++ Grid.java
@@ -0,0 +1,80 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Grid {
+    boolean[][] grid;
+    static final int WIDTH = 20;
+    static final int HEIGHT = 10;
+    boolean[][] currentBlock;
+    public int g = 1;
+    Timer timer = new Timer();
+
+    public Grid() {
+        this.grid = new boolean[10][20];
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 20; k++){
+                grid[i][k] = false;
+            }
+        }
+    }
+    public boolean[][] getGrid() {
+        return this.grid;
+    }
+    public void print() {
+        for (int i = 0; i<10; i++){
+            for (int k = 0; k < 20; k++){
+                if (grid[i][k] == false) {
+                    System.out.print("0");
+                } else {
+                    System.out.print("1");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+    public void fillFirst() {
+        int counter = 0;
+        for (int k = 0; k < 20; k++) {
+            if (grid[0][k]) {
+                counter += 1;
+            }
+        }
+        if (counter == 0) {
+            block random = new block();
+            boolean[][] anotherOne = random.selectRandom();
+            currentBlock = anotherOne;
+            for (int i = 0; i < 2; i++) {
+                for (int k = 0; k < 3; k++) {
+                    grid[i][(WIDTH/2)-1+k] = anotherOne[i][k];
+
+                }
+            }
+        }
+    }
+    public void fillLower() {
+        for (int i = 0; i < 2; i++) {
+            for (int k = 0; k < 3; k++) {
+                grid[g][(WIDTH/2)-1+k] = currentBlock[i][k];
+                grid[g-1][(WIDTH/2)-1+k] = false;
+            }
+        }
+        print();
+        System.out.println();
+        g += 1;
+        if (g == 10) {
+            g = 1;
+        }
+    }
+
+    public void fallingblocks() {
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+               fillLower();
+            }
+        }, 0, 1000);
+    }
+}

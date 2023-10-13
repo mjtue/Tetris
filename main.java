354de5dea694aa397696233d17f11c8c9cea8125
mjtue <147407969+mjtue@users.noreply.github.com>
2023-10-13 14:38:20 +0200

    Update main.java
    Main class configured after changing the method responsible for the blocks falling

--- main.java
+++ main.java
@@ -2,28 +2,22 @@ import javax.swing.*;
 
 public class main {
 
-
-
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("new tetris game", null);
+        JFrame frame = new JFrame("CBL tetris game", null);
         
-        
-        frame.setLocationRelativeTo(null);
-        frame.setSize(500, 500);
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel();
-        JButton button = new JButton("start", null);
-
-        // button.setBounds(100, 100, 50, 50);
-        
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+        MainPanel panel = new MainPanel();
         frame.add(panel);
-        panel.add(button);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
 
-
+        Grid grid = new Grid();
+        grid.fillFirst();
+    
+        grid.fallingblocks();
     }
 }
 

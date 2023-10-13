220b5a1418050b1b0f38bd0f84394629d5ae79e2
mjtue <147407969+mjtue@users.noreply.github.com>
2023-10-13 14:41:36 +0200

    Create blocks.java

---
+++ blocks.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class block {
+    Random rand = new Random();
+
+    public boolean[][] selectRandom() {
+        int random = rand.nextInt(3);
+        if (random == 0) {
+            return KindsOfBlocks.block0();
+        } else if (random == 1) {
+            return KindsOfBlocks.block1();
+        } else if (random == 2) {
+            return KindsOfBlocks.block2();
+        } else {
+            return KindsOfBlocks.block3();
+        }
+    }
+}
+

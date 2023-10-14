b8eb585e27f28b51ee0cc78a4f747c925f57e656
kapalaf <147754212+kapalaf@users.noreply.github.com>
2023-10-14 19:45:47 +0200

    Update blocks.java

--- blocks.java
+++ blocks.java
@@ -4,15 +4,19 @@ public class block {
     Random rand = new Random();
 
     public boolean[][] selectRandom() {
-        int random = rand.nextInt(4);
+        int random = rand.nextInt(6);
         if (random == 0) {
             return KindsOfBlocks.block0();
         } else if (random == 1) {
             return KindsOfBlocks.block1();
         } else if (random == 2) {
             return KindsOfBlocks.block2();
-        } else {
+        } else if (random == 3) {
             return KindsOfBlocks.block3();
+        } else if (random == 4) {
+            return KindsOfBlocks.block4();
+        } else {
+            return KindsOfBlocks.block5();
         }
     }
 }

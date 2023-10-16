c1bc5b6572f9a14cdc79755d9d19b18b5b35778e
mjtue <147407969+mjtue@users.noreply.github.com>
2023-10-16 18:21:25 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,29 +1,29 @@
+
+
+import java.awt.BorderLayout;
+
 import javax.swing.*;
+
+
 
 public class main {
 
-
-
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("new tetris game", null);
+        JFrame frame = new JFrame("CBL tetris game", null);
         
-        
-        frame.setLocationRelativeTo(null);
-        frame.setSize(500, 500);
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel();
-        JButton button = new JButton("start", null);
-
-        button.setBounds(100, 100, 50, 50);
-        
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+        MainPanel panel = new MainPanel();
         frame.add(panel);
-        panel.add(button);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
 
+        Grid grid = new Grid();
+        GameManager manager = new GameManager();
 
+        manager.run(grid);
     }
 }
 

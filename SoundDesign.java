bc8400a9f11e0ac37590f37740849a6cdf5a22c6
mjtue <147407969+mjtue@users.noreply.github.com>
2023-10-23 03:15:49 +0200

    Update and rename SettingsFrame.java to SoundDesign.java
    Changes mostly consisting of changing the class that initially was supposed to be a frame simply to a class responsible for methods focused on adjusting the soundtrack

---
+++ SoundDesign.java
@@ -0,0 +1,79 @@
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class SoundDesign {
+    Clip clip;
+    float current = 0;
+    float previous = 0;
+    FloatControl fc;
+    boolean mute = false;
+    URL sound = getClass().getResource("resources/Free Music - Tetris (Dark Version) (No Copyright Music).wav");
+
+    public SoundDesign() {
+        setFile(sound);
+    }
+
+    public void setFile(URL url) {
+        try {
+            AudioInputStream sound = AudioSystem.getAudioInputStream(url);
+            clip = AudioSystem.getClip();
+            clip.open(sound);
+            fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        }
+        catch (Exception e) {
+            JFrame frame = new JFrame();
+            JOptionPane.showMessageDialog(frame,"The game was unable to download all the music files, make sure to download all of the game files",
+               "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+        
+    }
+    public void play (URL url) {
+        clip.setFramePosition(0);
+        clip.start();
+    }
+    public void loop (URL url) {
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+    public void stop (URL url) {
+        clip.stop();
+    }
+    public void SoundMute() {
+        if (mute == false) {
+            previous = current;
+            current = -80.0f;
+            fc.setValue(current);
+            mute = true;
+        } else if (mute == true) {
+            current = previous;;
+            fc.setValue(current);
+            mute = false;
+        }
+    }
+    public void downVolume() {
+        current -= 3.0f;
+        if (current < - 80.0f) {
+            current = -80.0f;
+        }
+        fc.setValue(current);
+    }
+    public void upVolume() {
+        current += 3.0f;
+        if (current > 6.0f) {
+            current = 6.0f;
+        }
+        fc.setValue(current);
+    }
+    
+}

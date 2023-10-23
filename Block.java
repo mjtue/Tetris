41a7bde84293b5a08de173aeaf6e929ba71af470
mjtue <147407969+mjtue@users.noreply.github.com>
2023-10-23 03:31:53 +0200

    Create Block.java
    class responsible for choosing a random block which is generated at the beginning of each turn

---
+++ Block.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Block {
+    Random rand = new Random();
+
+    public boolean[][] selectRandom() {
+        int random = rand.nextInt(6);
+        if (random == 0) {
+            return KindsOfBlocks.block0();
+        } else if (random == 1) {
+            return KindsOfBlocks.block1();
+        } else if (random == 2) {
+            return KindsOfBlocks.block2();
+        } else if (random == 3) {
+            return KindsOfBlocks.block3();
+        } else if (random == 4) {
+            return KindsOfBlocks.block4();
+        } else {
+            return KindsOfBlocks.block5();
+        }
+    }
+}

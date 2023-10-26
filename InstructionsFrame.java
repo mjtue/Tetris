909eda2681e90102b8034875e873051d5e42f2a8
kapalaf <147754212+kapalaf@users.noreply.github.com>
2023-10-26 13:55:29 +0200

    Update InstructionsFrame.java
    cleaning the code

--- InstructionsFrame.java
+++ InstructionsFrame.java
@@ -1,8 +1,5 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;

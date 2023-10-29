d32ad311f53754be4d63ab81c4143c79f1475c28
kapalaf <147754212+kapalaf@users.noreply.github.com>
2023-10-29 19:25:24 +0100

    Update SoundDesign.java
    comments added

--- SoundDesign.java
+++ SoundDesign.java
@@ -1,108 +1,89 @@
-import java.net.URL;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+/**Class responsible for displaying five best user's scores.
+ * 
+ */
+public class ScoreBoard extends JFrame {
+    File file = new File("highscores.txt");
+    ArrayList<Integer> scores = new ArrayList<>();
 
-public class SoundDesign {
-    Clip clip;
-    float current = 0;
-    float previous = 0;
-    FloatControl fc;
-    boolean mute = false;
-    URL sound = getClass().getResource("resources/Free Music - "
-                                        +
-                                    "Tetris (Dark Version) (No Copyright Music).wav");
-
-    public SoundDesign() {
-        setFile(sound);
-    }
-
-    /** Setting the music file.
-     * 
-     * Setting the audio file which then can be adjusted by the FloatControl to set a specific
-     * Volume of music
-     * 
-     * @param url the audio file with the music
-     */
-    public void setFile(URL url) {
-        try {
-            AudioInputStream sound = AudioSystem.getAudioInputStream(url);
-            clip = AudioSystem.getClip();
-            clip.open(sound);
-            fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-        } catch (Exception e) {
-            JFrame frame = new JFrame();
-            JOptionPane.showMessageDialog(frame, "The game was unable to download all the music"
-                                                 +
-                                            "files, make sure to download all of the game files",
-                                        "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
+    public ScoreBoard() {
+        setSize(500, 500);
+        setTitle("Scoreboard");
+        JPanel panel = new JPanel();
         
-    }
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                
+                    add(new JPanel() {
+                        @Override
+                        public Dimension getPreferredSize() {
+                            return new Dimension(100, 50);
+                        }
+                    });
+                add(panel, BorderLayout.CENTER);
+            } 
+        });
+        panel.setLayout(new GridLayout(0, 1));
 
-    /** When the method is called the music starts playing.
-     * 
-     * 
-     * @param url the audio file which is assigned URL as the parameter 
-     *            inside the method is played when the method is called.
-     */
-    public void play(URL url) {
-        clip.setFramePosition(0);
-        clip.start();
-    }
+        // reading through the file containing the scores and adding them to the ArrayList (scores)
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            reader.readLine();
+            String line = reader.readLine();
+        
+            while (line != null) {
+                scores.add(Integer.parseInt(line.trim()));
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException ex) {
+            System.out.println("ERROR while reading the files from the file");
+        }
 
-    public void loop(URL url) {
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
-    }
+        // using bubble sort algorithm in order to sort the scores of the arrayList
+        // to make them appear in decreasing order
+        for (int i = 0; i < scores.size(); i++) {
+            for (int current = 0; current < scores.size() - 1 - i; current++) {
+                if (scores.get(current) < scores.get(current + 1)) {
+                    int temp = scores.get(current);
+                    scores.set(current, scores.get(current + 1));
+                    scores.set(current + 1, temp);
+                }
+            }
+        }
 
-    public void stop(URL url) {
-        clip.stop();
-    }
 
-    /**Method responsible for muting and unmuting the music.
-     * 
-     * dependent on the value of boolean mute the the music
-     * is either muted or when the method is called again 
-     * the music is unmuted. 
-     * 
-     */
-    public void soundMute() {
-        if (mute) {
-            current = previous;;
-            fc.setValue(current);
-            mute = false;
+        // the conditional statements used to only display best 5 of player's scores
+        if (scores.size() < 5) {
+            for (int i  = 0; i < scores.size(); i++) {
+                String score = String.valueOf(scores.get(i));
+                JLabel label  = new JLabel((i + 1) + ". " + score);
+                label.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30));
+                panel.add(label);   
+            }
         } else {
-            previous = current;
-            current = -80.0f;
-            fc.setValue(current);
-            mute = true;
+            for (int i  = 0; i < 5; i++) {
+                String score = String.valueOf(scores.get(i));
+                JLabel label  = new JLabel((i + 1) + ". " + score);
+                label.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30));
+                panel.add(label);   
+            }
         }
     }
 
-    /** Turning the music volume down.
-     *
-     * Volume of the music is lowered by 3.0f each time the 
-     * method is called.
-     * 
-     */
-    public void downVolume() {
-        current -= 3.0f;
-        fc.setValue(current);
-    }
-
-    /** Turning the music volume up.
-     * 
-     * Each time the method is called volume of the music
-     * is increased by 3.0f.
-     * 
-     */
-    public void upVolume() {
-        current += 3.0f;
-        fc.setValue(current);
-    }
     
 }
